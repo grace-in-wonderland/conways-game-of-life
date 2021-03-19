6a170e02c734eaf579df043687bd3b2b25636ef6
Grace McPherson <grace.theprogrammer@gmail.com>
2021-03-19 13:13:53 -0400

    added preset patterns to interact with

--- lib/conways_game_of_life/grid.ex
+++ lib/conways_game_of_life/grid.ex
@@ -33,6 +33,51 @@ defmodule Grid do
     end
   end
 
+  # still life pattern
+  def loaf({x,y}) do
+    [       {1,0}, {2,0},
+      {0,1},             {3,1},
+      {0,2},       {2,2},
+            {1,3}]
+    |> move({x,y})
+  end
+
+  #oscillator pattern
+  def blinker({x,y}) do
+    [ {0,0}, {1,0}, {2,0} ]
+    |> move({x,y})
+  end
+
+  #oscillator pattern
+  def pulsar({x,y}) do
+    [              {2,0}, {3,0}, {4,0},
+      {0,1},                            {5,1},
+      {0,2},                            {5,2},
+      {0,3},                            {5,3},
+
+                   {2,5}, {3,5}, {4,5}]
+    |> move({x,y})
+  end
+
+  #spaceship pattern
+  def glider({x,y}) do
+    [{0,0}, {1,0}, {2,0},
+                   {2,1},
+            {1,2}]
+    |> move({x,y})
+  end
+
+  #spaceship pattern
+  def lightweight_space_ship({x,y}) do
+    [       {1,0},               {4,0},
+      {0,1},
+      {0,2},                     {4,2},
+      {0,3}, {1,3}, {2,3}, {3,3}]
+    |> move({x,y})
+  end
+
+  defp move(points, {delta_x, delta_y}), do: Enum.map(points, fn {x, y} -> {x + delta_x, y + delta_y} end)
+
   defp generate_neighbors({x, y}, :keep), do: [{x,y}, {x+1, y}, {x-1, y}, {x, y+1}, {x, y-1}, {x+1, y+1}, {x-1, y+1}, {x+1, y-1}, {x-1, y-1}]
   defp generate_neighbors({x, y}), do: [{x+1, y}, {x-1, y}, {x, y+1}, {x, y-1}, {x+1, y+1}, {x-1, y+1}, {x+1, y-1}, {x-1, y-1}]
 end
--- lib/conways_game_of_life_web/live/life_live.ex
+++ lib/conways_game_of_life_web/live/life_live.ex
@@ -10,6 +10,10 @@ defmodule ConwaysGameOfLifeWeb.LifeLive do
   def render(assigns) do
     ~L"""
       <button phx-click="next_turn">Next</button>
+      <button phx-click="add_blinker">Blinker</button>
+      <button phx-click="add_pulsar">Pulsar</button>
+      <button phx-click="add_glider">Glider</button>
+      <button phx-click="add_spaceship">Space Ship</button>
 
       <svg width="400" height="400" style="margin-left: 25%; background-color: #eeeeee">
         <defs>
@@ -29,6 +33,30 @@ defmodule ConwaysGameOfLifeWeb.LifeLive do
   def handle_event("next_turn", _params, socket ) do
     current_cells = socket.assigns.alivecells
     {:noreply, assign(socket, alivecells: (Grid.turn(current_cells)) )}
+  end
+
+  def handle_event("add_blinker", _params, socket) do
+    current_cells = socket.assigns.alivecells
+    blinker_cells = Grid.blinker({15, 15})
+    {:noreply, assign(socket, alivecells: current_cells ++ blinker_cells)}
+  end
+
+  def handle_event("add_pulsar", _params, socket) do
+    current_cells = socket.assigns.alivecells
+    pulsar_cells = Grid.pulsar({5, 25})
+    {:noreply, assign(socket, alivecells: current_cells ++ pulsar_cells)}
+  end
+
+  def handle_event("add_glider", _params, socket) do
+    current_cells = socket.assigns.alivecells
+    loaf_cells = Grid.glider({5, 35})
+    {:noreply, assign(socket, alivecells: current_cells ++ loaf_cells)}
+  end
+
+  def handle_event("add_spaceship", _params, socket) do
+    current_cells = socket.assigns.alivecells
+    spaceship_cells = Grid.lightweight_space_ship({30, 2})
+    {:noreply, assign(socket, alivecells: current_cells ++ spaceship_cells)}
   end
 
   defp x({x,_y}), do: (x-1)*10
